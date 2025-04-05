5607d999cfbfb2f0b281808ffa9bb8c75a1b0f55
antonyxs28 <antonysantos3286@gmail.com>
2025-04-05 20:36:01 -0300

    feat: not found

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export function NotFound() {
-  return <h1>not found page</h1>
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404</h1>
+      <p>page not found</p>
+      <Link to="/" style={{ textDecoration: 'none', color: 'blue' }}>
+        to back
+      </Link>
+    </div>
+  );
 }
