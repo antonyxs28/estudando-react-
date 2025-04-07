23db15ecca650d3f601ed9b7d7f57665a58f0fd9
antonyxs28 <antonysantos3286@gmail.com>
2025-04-07 15:48:21 -0300

    useState

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
+interface User {
+  name: string;
+  login: string;
+  avatar_url: string;
+}
 
 export function HomePage() {
+  const [users, setUsers] = useState<User[]>();
+  async function loadData() {
+    const response = await fetch("https://api.github.com/users/antonysantos71");
+    const data = await response.json();
+    setUsers(data);
+  }
+  loadData();
+
   return (
     <div>
       <h1>home</h1>
       <div>
-        <NavLink to="/login" >login</NavLink>
+        {users?.map((user) => (
+          <div>{user.name}</div>
+        ))}
       </div>
     </div>
   );
